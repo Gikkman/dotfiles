351c3200cd27e4bd5e2439db91faaa3366713882
Gikkman <ksr.jonasson@gmail.com>
2020-04-05 11:46:56 +0200

    add alias for starting a server in current directory

--- source/alias.sh
+++ source/alias.sh
@@ -1,3 +1,5 @@
 alias ll='gls -lah --color --group-directories-first'
 alias ..='cd ..'
 alias ....='cd .. && cd ..'
+
+alias server='printf "Please visit http://localhost:8080\n" && python -m SimpleHTTPServer 8080'
