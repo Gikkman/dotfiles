80bb176b64fe52cce6f3fa10e799ececb9fad1c8
gikkman <gikk@gikkman.com>
2022-02-22 23:28:16 +0100

    Update alias

--- source/alias.sh
+++ source/alias.sh
@@ -1,7 +1,12 @@
 alias ..='cd ..'
 alias ....='cd .. && cd ..'
 alias ......='cd .. && cd .. && cd ..'
-alias ll='ls -lah --color=yes --group-directories-first'
 
 alias gp='git pull'
 alias gc='git checkout'
+
+if $(type gls > /dev/null); then
+    alias ll="gls -lah --color=yes --group-directories-first"
+else 
+    alias ll="ls -lah --color=yes --group-directories-first"
+fi
